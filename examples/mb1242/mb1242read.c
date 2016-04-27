fb793fe7654594a7aada89456cc075ccf4bf0d60
simondlevy <simon.d.levy@gmail.com>
2016-04-27 16:49:12 -0400

    j q

---
+++ examples/mb1242/mb1242read.c
@@ -0,0 +1,32 @@
+/*
+   mb1242_read : read values from MaxBotix MB1242 I^2C sonar
+
+   Copyright (C) 2016 Simon D. Levy 
+
+   This file is part of BreezySTM32.
+
+   BreezySTM32 is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BreezySTM32 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <breezystm32.h>
+
+void setup(void)
+{
+    i2cInit(I2CDEV_2);
+} 
+
+void loop(void)
+{
+    printf("%d\n", mb1242_poll());
+}
