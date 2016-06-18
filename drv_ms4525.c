1a9bdc9bab2fcbb34b53afcc0d5b328f340505e1
James Jackson <superjax08@gmail.com>
2016-06-18 13:11:28 -0600

    added licenses

--- drv_ms4525.c
+++ drv_ms4525.c
@@ -1,9 +1,24 @@
-#include <stdint.h>
-#include <stdbool.h>
+/*
+   drv_ms4525.c : driver for MS4525 differential pressure sensor
+
+   This file is part of BreezySTM32.
+
+   BreezySTM32 is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BreezySTM32 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include <breezystm32.h>
-
-#include "drv_ms4525.h"
 
 // MS4525 address 0x28 for most common version
 #define MS4525_ADDR   0x28
--- drv_ms4525.h
+++ drv_ms4525.h
@@ -1,6 +1,23 @@
-#pragma once
+/*
+   drv_ms4525.h : driver for MS4525 Differential Pressure Sensor
 
-#include "drv_i2c.h"
+   This file is part of BreezySTM32.
+
+   BreezySTM32 is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BreezySTM32 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 bool ms4525_detect(void);
 void ms4525_init(void);
