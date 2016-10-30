2120e9973d6033fb3797bbfde3f103ec32067fb3
simondlevy <simon.d.levy@gmail.com>
2016-10-29 22:06:28 -0400

    More cleanup

--- f3/io.c
+++ f3/io.c
@@ -2,8 +2,31 @@
 #include <stddef.h>
 
 #include "io.h"
-#include "io_impl.h"
 
+typedef struct ioDef_s {
+    ioTag_t tag;
+} ioDef_t;
+
+typedef struct ioRec_s {
+    GPIO_TypeDef *gpio;
+    uint16_t pin;
+    resourceOwner_t owner;
+    resourceType_t resourcesUsed; // TODO!
+} ioRec_t;
+
+extern ioRec_t ioRecs[DEFIO_IO_USED_COUNT];
+
+int IO_GPIOPortIdx(IO_t io);
+int IO_GPIOPinIdx(IO_t io);
+#if defined(STM32F10X)
+int IO_GPIO_PinSource(IO_t io);
+int IO_GPIO_PortSource(IO_t io);
+#elif defined(STM32F303xC)
+int IO_EXTI_PortSourceGPIO(IO_t io);
+int IO_EXTI_PinSource(IO_t io);
+#endif
+uint32_t IO_EXTI_Line(IO_t io);
+ioRec_t *IO_Rec(IO_t io);
 enum rcc_reg {
     RCC_EMPTY = 0,   // make sure that default value (0) does not enable anything
     RCC_AHB,
