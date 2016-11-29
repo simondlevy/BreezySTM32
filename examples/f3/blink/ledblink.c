a470850ba806ba7c0612bd076f0096af27707254
simondlevy <simon.d.levy@gmail.com>
2016-11-29 17:32:04 -0500

    Restored F3 blink example

---
+++ examples/f3/blink/ledblink.c
@@ -0,0 +1,32 @@
+/*
+   ledblink.c : blink the LED
+
+   Copyright (C) 2016 Simon D. Levy 
+
+   This file is part of BreezySTM32.
+
+   BreezySTM32 is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BreezySTM32 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <breezystm32.h>
+
+void setup(void)
+{
+} 
+
+void loop(void)
+{
+    LED0_TOGGLE;
+    delay(500);
+}
