84b7396cf163917fc517feb7667833ff8bbe0067
simondlevy <simon.d.levy@gmail.com>
2016-12-14 14:32:56 -0500

    Polling, so don't enable interrupt

--- examples/mpu6050_poll/mpu6050_poll.c
+++ examples/mpu6050_poll/mpu6050_poll.c
@@ -30,7 +30,7 @@ void setup(void)
 
     uint16_t acc1G;
     float gyro_scale; // converts to units of rad/s
-    mpu6050_init(true, &acc1G, &gyro_scale, BOARD_REV);
+    mpu6050_init(false, &acc1G, &gyro_scale, BOARD_REV);
 }
 
 void loop(void)
