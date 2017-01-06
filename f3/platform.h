2cbd78c06c68ffbc012aa82a12732d8d5e845c79
simondlevy <simon.d.levy@gmail.com>
2017-01-06 00:17:45 -0500

    Cleanup

--- f3/platform.h
+++ f3/platform.h
@@ -1,12 +1,11 @@
 #pragma once
 
-#ifdef STM32F303xC
 #include "stm32f30x_conf.h"
 #include "stm32f30x_rcc.h"
 #include "stm32f30x_gpio.h"
 #include "core_cm4.h"
+
 #define STM32F3
-#endif
 
 #include "target.h"
 
