48df1c40d909915a42455c8323285a968ce115ec
simondlevy <simon.d.levy@gmail.com>
2017-02-01 23:57:21 -0500

    created header file

--- f3/new/src/main.c
+++ f3/new/src/main.c
@@ -1,33 +1,4 @@
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include <stdio.h>
-
-#include <platform.h>
-
-#include "config/parameter_group.h"
-#include "config/parameter_group_ids.h"
-
-#include "drivers/system.h"
-#include "drivers/dma.h"
-#include "drivers/gpio.h"
-#include "drivers/timer.h"
-#include "drivers/pwm_mapping.h"
-#include "drivers/pwm_output.h"
-#include "drivers/pwm_rx.h"
-#include "drivers/io.h"
-#include "drivers/serial.h"
-#include "drivers/serial_usb_vcp.h"
-
-#include "io/motors.h"
-
-//#include "flight/mixer.h"
-
-#include "config/config_system.h"
-#include "config/config_eeprom.h"
-#include "config/profile.h"
+#include "breezystm32.h"
 
 // from system_stm32f30x.c
 void SetSysClock(void);
