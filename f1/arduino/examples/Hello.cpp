c6332796985221a79590000bb26e7446bbbcbd2d
simondlevy <simon.d.levy@gmail.com>
2017-02-03 16:28:31 -0500

    Begin Hello World example

---
+++ f1/arduino/examples/Hello.cpp
@@ -0,0 +1,17 @@
+extern "C" {
+ 
+#include <Arduino.h>
+
+// the setup routine runs once when you press reset:
+void setup() {                
+
+    //Serial.begin(115200);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+    //Serial.printf("%d\n", millis());
+}
+
+}
