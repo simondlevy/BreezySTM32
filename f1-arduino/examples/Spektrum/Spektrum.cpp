6ad110d2953fb7ce59a9713f5afd68b1e2564b29
simondlevy <simon.d.levy@gmail.com>
2017-02-08 16:48:31 -0500

    Works with new Arduino SpektrumDSM

--- f1-arduino/examples/Spektrum/Spektrum.cpp
+++ f1-arduino/examples/Spektrum/Spektrum.cpp
@@ -1,23 +1,45 @@
 /*
-   Spektrum demonstrates Spektrum serial receiver
+   ReadSpektrum.ino : example of how to read from a Spektrum DSM receiver using Arduino
+
+   This file is part of SpektrumDSM.
+
+   SpektrumDSM is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include <Arduino.h>
 #include <SpektrumDSM.h>
 
-SpektrumDSM2048 rx;
+SpektrumDSM rx;
 
+static uint8_t chanmap[5] = {1, 2, 3, 0, 5};
 
-// the setup routine runs once when you press reset:
-void setup() {                
-    Serial.begin(115200);
-    rx.begin();
+void setup() {
+  
+  Serial.begin(115200);
+  
+  rx.begin(SERIALRX_SPEKTRUM2048);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-    for (int k=0; k<6; ++k) {
-        Serial.printf("%d ", rx.getChannelValue(k));
+
+    if (rx.frameComplete()) {
+        for (int k=0; k<5; ++k)
+            Serial.printf("%d ", rx.readRawRC(chanmap[k]));
+        Serial.printf("\n");
     }
-    Serial.printf("\n");
+
+    // Allow some time between readings
+    delay(10);  
+
+    
 }
