77412e8a197b2f38964712283b26a1a14c0d6b7f
simondlevy <simon.d.levy@gmail.com>
2017-02-14 12:48:20 -0500

    Blinky working with Arduino API

--- f3/Arduino.cpp
+++ f3/Arduino.cpp
@@ -1,5 +1,6 @@
 extern "C" {
 
+#include <Arduino.h>
 #include "breezystm32.h"
 
 // from system_stm32f30x.c
@@ -24,6 +25,53 @@ void debug(const char * fmt, ...)
     
     while (!isSerialTransmitBufferEmpty(Serial1));
 }
+
+static GPIO_TypeDef * gpio_type_from_pin(uint8_t pin)
+{
+    return  pin == 8 ? LED0_GPIO : LED1_GPIO;
+}
+
+static uint16_t gpio_pin_from_pin(uint8_t pin)
+{
+    return pin == 8 ? LED0_PIN  : LED1_PIN;
+}
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+    // XXX currently support output mode only
+    if (mode != OUTPUT) return;
+
+    pin = 1<<pin;
+
+    GPIO_TypeDef * gpio = gpio_type_from_pin(pin);
+
+    gpio_config_t cfg;
+
+    cfg.pin = gpio_pin_from_pin(pin);
+    cfg.mode = Mode_Out_PP;
+    cfg.speed = Speed_2MHz;
+
+    gpioInit(gpio, &cfg);
+}
+
+void digitalWrite(uint8_t pin, uint8_t level)
+{
+    pin = 1<<pin;
+
+    GPIO_TypeDef * gpio = gpio_type_from_pin(pin);
+
+    uint16_t gpio_pin = gpio_pin_from_pin(pin);
+
+    switch (level) {
+        case HIGH:
+            digitalLo(gpio, gpio_pin);
+            break;
+        case LOW:
+            digitalHi(gpio, gpio_pin);
+            break;
+    }
+}
+
 
 int main(void) {
 
--- f3/examples/Blink/Blink.cpp
+++ f3/examples/Blink/Blink.cpp
@@ -1,19 +1,33 @@
-extern "C" {
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+
+// Pin 13 has an LED connected on most Arduino boards.
+// Pin 11 has the LED on Teensy 2.0
+// Pin 6  has the LED on Teensy++ 2.0
+// Pin 13 has the LED on Teensy 3.0
+// Pin 8 has LED 0 on Naze
+// Pin 16 has LED 0 on Naze
+// give it a name:
+int led = 16;
 
 #include <Arduino.h>
-#include <breezystm32.h>
-#include <light_led.h>
 
+// the setup routine runs once when you press reset:
+void setup() {                
 
-void setup(void)
-{
-    ledInit(false);
+    // initialize the digital pin as an output.
+    pinMode(led, OUTPUT);     
 }
 
-void loop(void)
-{
-    LED0_TOGGLE;
-    delay(500);
-}
+// the loop routine runs over and over again forever:
+void loop() {
 
-} // extern "C"
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);               // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);               // wait for a second
+}
