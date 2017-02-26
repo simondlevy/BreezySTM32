a37dd92838cca6481d4536ba381586851d31b58c
Simon D. Levy <simon.d.levy@gmail.com>
2017-02-26 14:36:39 -0500

    Cleanup

--- f3/light_led_stm32f30x.c
+++ f3/light_led_stm32f30x.c
@@ -38,7 +38,7 @@ void ledInit(void)
     RCC_AHBPeriphClockCmd(LED0_PERIPHERAL_2, ENABLE);
     led_config[0].gpio = LED0_GPIO_2;
     led_config[0].pin = LED0_PIN_2;
-    cfg.pin = led_config[0].pin;
+    cfg.pin = LED0_PIN_2; //led_config[0].pin;
     LED0_OFF;
     gpioInit(led_config[0].gpio, &cfg);
 }
