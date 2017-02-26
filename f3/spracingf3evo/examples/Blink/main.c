250b25bd5acfc35cdbfa4d0fcdd6e4742f253ac6
Simon D. Levy <simon.d.levy@gmail.com>
2017-02-26 14:18:21 -0500

    Blinky partly working on SPRACINGF3EVO

---
+++ f3/spracingf3evo/examples/Blink/main.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+
+#include "platform.h"
+#include "system.h"
+#include "dma.h"
+#include "gpio.h"
+#include "timer.h"
+#include "serial.h"
+#include "serial_uart.h"
+#include "exti.h"
+#include "light_led.h"
+
+void SetSysClock(void);
+
+int main(void) {
+
+    // start fpu
+    SCB->CPACR = (0x3 << (10*2)) | (0x3 << (11*2));
+
+    SetSysClock();
+
+    systemInit();
+
+    timerInit();  // timer must be initialized before any channel is allocated
+
+    dmaInit();
+
+    ledInit(true);
+
+    while (true) {
+        LED0_ON;
+    }
+
+} // main
