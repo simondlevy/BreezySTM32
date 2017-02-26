1f198b61896753f137f44b6bc4f172067422d014
Simon D. Levy <simon.d.levy@gmail.com>
2017-02-26 12:24:59 -0500

    Refactored F3 directories

---
+++ f3/spracingf3evo/examples/Hello/Hello.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+// the setup routine runs once when you press reset:
+void setup() {                
+
+    Serial.begin(115200);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+    Serial.printf("%d\n", millis());
+}
