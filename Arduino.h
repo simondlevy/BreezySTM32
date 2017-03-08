89d264db163e8652f64067fa136a438202ccafd6
simondlevy <simon.d.levy@gmail.com>
2017-03-08 17:05:28 -0500

    Added "#pragma once"

--- Arduino.h
+++ Arduino.h
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License
 along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 extern "C" {
 
 #include <stdint.h>
